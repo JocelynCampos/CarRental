c682085df6aafcd9d4e5f87350e6682ba026fc6b
Jocelyn <aracely.campos@hotmail.com>
2025-04-23 19:29:16 +0200

    data.sql + totalcost

--- src/main/java/se/edugrade/carrental/entities/User.java
+++ src/main/java/se/edugrade/carrental/entities/User.java
@@ -3,7 +3,7 @@ package se.edugrade.carrental.entities;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User
 {
     @Id
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO user (id, social_security_number, first_name, last_name, address, email, phone_number, role) VALUES
+INSERT INTO users (id, social_security_number, first_name, last_name, address, email, phone_number, role) VALUES
 (1, '19850101-1234', 'Anna', 'Svensson', 'Eduvägen 40', 'anna.svensson@gmail.com', '070-9638527', 'CUSTOMER'),
 (2, '19900215-5678', 'Erik', 'Johansson', 'Granvägen 50', 'erik.j@outlook.se', '072-3692587', 'CUSTOMER'),
 (3, '19751530-9101', 'Maria', 'Lindberg', 'Granitvägen 1', 'lindberg1@live.se', '076-7418529', 'CUSTOMER'),
