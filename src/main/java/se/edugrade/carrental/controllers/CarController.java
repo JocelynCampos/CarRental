608349ebb4ab4b584e11306be2fdb7e0657487d6
Rancibi <rancibi.business@gmail.com>
2025-04-24 11:41:31 +0200

    Lagt till logic för Car service och api

--- src/main/java/se/edugrade/carrental/controllers/CarController.java
+++ src/main/java/se/edugrade/carrental/controllers/CarController.java
@@ -1,9 +1,53 @@
 package se.edugrade.carrental.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import se.edugrade.carrental.entities.Car;
+import se.edugrade.carrental.services.CarServiceInterface;
 
-@Controller
+import java.util.List;
+
+// Hugo Ransvi
+
+@RestController
+@RequestMapping("/api/v1")
 public class CarController
 {
+    @Autowired
+    private CarServiceInterface carService;
 
+    // Kund Endpoints
+
+    @GetMapping("/cars")
+    public List<Car> getAvailableCars() {
+        return carService.getAvailableCars();
+    }
+
+    // Admin Endpoints
+
+    @GetMapping("/admin/cars")
+    public List<Car> getAvailableCarsAdmin() {
+        return carService.getAvailableCars();
+    }
+
+    @GetMapping("/admin/allcars")
+    public List<Car> getAllCars() {
+        return carService.getAllCars();
+    }
+
+    @PostMapping("/admin/addcar")
+    public Car addCar(@RequestBody Car car) {
+        return carService.addCar(car);
+    }
+
+    @PutMapping("/admin/updatecar")
+    public Car updateCar(@RequestBody Car car) {
+        return carService.updateCar(car);
+    }
+
+    @DeleteMapping("/admin/deletecar")
+    public void deleteCar(@RequestParam Long id) {
+        carService.removeCar(id);
+    }
 }
--- src/main/java/se/edugrade/carrental/services/CarServiceInterface.java
+++ src/main/java/se/edugrade/carrental/services/CarServiceInterface.java
@@ -1,6 +1,16 @@
 package se.edugrade.carrental.services;
 
+import se.edugrade.carrental.entities.Car;
+
+import java.util.List;
+
+// Hugo Ransvi
+
 public interface CarServiceInterface
 {
-
+    List<Car> getAvailableCars();
+    List<Car> getAllCars();
+    Car addCar(Car car);
+    Car updateCar(Car car);
+    void removeCar(Long id);
 }
