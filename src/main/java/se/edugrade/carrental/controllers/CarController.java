1f01c30ec9817cec9616e1e3efc1f883f3bcd570
Rancibi <rancibi.business@gmail.com>
2025-05-08 10:35:25 +0200

    Lite ändring i endpointsen

--- src/main/java/se/edugrade/carrental/controllers/CarController.java
+++ src/main/java/se/edugrade/carrental/controllers/CarController.java
@@ -1,10 +1,10 @@
 package se.edugrade.carrental.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
 import se.edugrade.carrental.entities.Car;
 import se.edugrade.carrental.vo.CarVO;
 import se.edugrade.carrental.services.CarServiceInterface;
@@ -23,23 +23,20 @@ public class CarController
     // Kund Endpoints
 
     @GetMapping("/cars")
-    public ResponseEntity<List<CarVO>> getAllPublicCars() {
-        List<CarVO> cars = carService.getAllCarsPublic();
-        return ResponseEntity.ok(cars);
+    public List<CarVO> getAllPublicCars() {
+        return carService.getAllCarsPublic();
     }
 
     // Admin Endpoints
 
     @GetMapping("/admin/cars")
-    public ResponseEntity<List<Car>> getAvailableCars() {
-        List<Car> cars = carService.getAvailableCars();
-        return ResponseEntity.ok(cars);
+    public List<Car> getAvailableCars() {
+        return carService.getAvailableCars();
     }
 
     @GetMapping("/admin/allcars")
-    public ResponseEntity<List<Car>> getAllCars() {
-        List<Car> cars = carService.getAllCars();
-        return ResponseEntity.ok(cars);
+    public List<Car> getAllCars() {
+        return carService.getAllCars();
     }
 
     @PostMapping("/admin/addcar")
@@ -50,8 +47,7 @@ public class CarController
 
     @PutMapping("/admin/updatecar")
     public ResponseEntity<Car> updateCar(@RequestBody Car car) {
-        Car updated = carService.updateCar(car);
-        return ResponseEntity.ok(updated);
+        return ResponseEntity.ok(carService.updateCar(car));
     }
 
     @DeleteMapping("/admin/removecar")
